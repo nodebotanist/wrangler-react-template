4826734d0281e4e5d1c1bba40ed801d51f187fb7
nodebotanist <the@nodebotani.st>
2019-09-18 10:18:50 -0500

    React installed

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const react = require('react')
+const reactDom = require('react-dom')
+const reactDomServer = require('react-dom/server')
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+	entry: "./index.js",
+	mode: "production",
+	optimization: {
+		minimize: true
+	},
+	performance: {
+		hints: false
+	},
+	output: {
+		path: __dirname + "/dist",
+		publicPath: "dist",
+		filename: "worker.js"
+	},
+	module: {
+		rules: [{
+			test: /\.m?js$/,
+			type: "javascript/auto",
+			use: {
+				loader: "babel-loader",
+				options: {
+					presets: ["@babel/preset-react"]
+				}
+			}
+		}]
+	}
+};
